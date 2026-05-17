4fd4525022f1954ef186db474bbfe4a064f4f784
FMIKKO\franc <francismikko.antonio@lgusuite.com>
2026-05-17 18:41:09 +0800

    feat: setup mongodb connection

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.json({
+    message: "Inventory System API Running",
+  });
+});
+
+export default app;
