acb0b0f50be017f818dc257c7bacf2e04bcf209c
FMIKKO\franc <francismikko.antonio@lgusuite.com>
2026-05-18 14:34:57 +0800

    implement authentication system

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,17 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 
+import authRoutes from "./routes/authRoutes";
+
 const app = express();
 
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
 
 app.get("/", (_req, res) => {
   res.json({
